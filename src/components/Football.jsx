8e7a262555f34edc4b996e4df51412a8f0b430a8
CloneTrooper66 <luka.eli88@gmail.com>
2024-06-21 10:24:39 +0100

    Sort articles

--- src/components/Football.jsx
+++ src/components/Football.jsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 export default function Football() {
   const [footballTopics, setFootballTopics] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [sort, setSort] = useState("a_to_z");
 
   useEffect(() => {
     setIsLoading(true);
@@ -22,28 +23,63 @@ export default function Football() {
       });
   }, []);
 
+  const sortedArticles = [...footballTopics].sort((a, b) => {
+    if (sort === "a_to_z") {
+      return a.title.localeCompare(b.title);
+    } else if (sort === "z_to_a") {
+      return b.title.localeCompare(a.title);
+    } else if (sort === "newest") {
+      return new Date(b.created_at) - new Date(a.created_at);
+    } else if (sort === "oldest") {
+      return new Date(a.created_at) - new Date(b.created_at);
+    } else if (sort === "most_votes") {
+      return b.votes - a.votes;
+    } else if (sort === "least_votes") {
+      return a.votes - b.votes;
+    }
+  });
+
   return isLoading ? (
     <Loading />
   ) : (
-    <div className="articles-wrapper">
-      {footballTopics.map((article) => (
-        <article className="article-container" key={article.article_id}>
-          <Link to={`/articles/${article.article_id}`}>
-            <p className="article-title">{article.title}</p>
-          </Link>
-          <img
-            src={article.article_img_url}
-            alt={article.title}
-            className="article-image"
-          />
-          <div className="article-details">
-            <p className="article-votes">Total Votes: {article.votes}</p>
-            <p className="article-created">
-              Created on {new Date(article.created_at).toLocaleDateString()}
-            </p>
-          </div>
-        </article>
-      ))}
-    </div>
+    <>
+      <div>
+        <br />
+        <label htmlFor="sort">Sort by: </label>
+        <select
+          id="sort"
+          value={sort}
+          onChange={(e) => setSort(e.target.value)}
+        >
+          <option value="a_to_z">Title: A - Z</option>
+          <option value="z_to_a">Title: Z - A</option>
+          <option value="newest">Newest First</option>
+          <option value="oldest">Oldest First</option>
+          <option value="most_votes">Most Votes</option>
+          <option value="least_votes">Least Votes</option>
+        </select>
+      </div>
+
+      <div className="articles-wrapper">
+        {sortedArticles.map((article) => (
+          <article className="article-container" key={article.article_id}>
+            <Link to={`/articles/${article.article_id}`}>
+              <p className="article-title">{article.title}</p>
+            </Link>
+            <img
+              src={article.article_img_url}
+              alt={article.title}
+              className="article-image"
+            />
+            <div className="article-details">
+              <p className="article-votes">Total Votes: {article.votes}</p>
+              <p className="article-created">
+                Created on {new Date(article.created_at).toLocaleDateString()}
+              </p>
+            </div>
+          </article>
+        ))}
+      </div>
+    </>
   );
 }
